de83c78dda5b38f7c84233fe3c97e492f1dc4596
praveenpckra <pravinpckra@gmail.com>
2022-11-23 16:13:28 +0530

    init

---
+++ mode.js
@@ -0,0 +1,4 @@
+function getval(){
+    console.log("I am Pravin");
+}
+module.export= getval();
---
+++ test.js
@@ -0,0 +1,4 @@
+// console.log("Hellow World");
+const mode =require ('./mode.js');
+var datformat =require('dateformat');
+mode;
